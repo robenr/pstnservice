5eaddf2848da901a6b10e94960cdde9a22e904b4
pradeep <pradeep@evalogical.com>
2019-08-06 18:21:33 +0530

    fixes

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
         <a class="navbar-brand brand-logo" href="{{url('portal')}}">PSTN Service</a>
-        <a class="navbar-brand brand-logo-mini" href="index.html">PSTN</a>
+        <a class="navbar-brand brand-logo-mini" href="{{url('portal')}}">PSTN</a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center">
         <ul class="navbar-nav ml-auto">
