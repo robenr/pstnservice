75b4ea7acf0f10b628fb7dc065bdaf07bdde99de
pradeep <pradeep@evalogical.com>
2019-08-20 19:53:16 +0530

    fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,7 +42,7 @@ class HomeController extends Controller
     public function submitcontactus(ContactUsRequest $request)
     {
         $user = $request->all();
-        Mail::to("testevauser@gmail.com")->send(new ContactUs($user));
+        Mail::to("mr.roben@gmail.com")->send(new ContactUs($user));
         return redirect('contactus');
     }
 
